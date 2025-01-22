da7270127101ca6586beeb31ca9e8a21882982db
AnanyaBansal-03 <ananyabansal3feb@gmail.com>
2025-01-22 22:02:38 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -6,7 +6,7 @@ int main() {
     if (isdigit(a)){
         printf("Digit");
     }
-    else if (a=='A' || 'E' || 'I' || 'O' || 'U' ||'a' || 'e' ||'i'||'o'||'u'){
+    else if (a=='A' || a== 'E' ||a== 'I' || a=='O' || a=='U' ||a=='a' ||a== 'e' ||a=='i'||a=='o'||a=='u'){
         printf("Vowel");
     }
     else if ((a>='A' && a<='Z') || (a>='a' && a<='z')){
