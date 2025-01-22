738402f738cca72228f098f69721916b5eb591a4
AnanyaBansal-03 <ananyabansal3feb@gmail.com>
2025-01-22 21:56:24 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -9,7 +9,7 @@ int main() {
     else if (a=="A" || "E" || "I" || "O" || "U" || "a" || "e" ||"i"||"o"||"u"){
         printf("Vowel");
     }
-    else if (a!="AEIOUaeiou"){
+    else if (a!="A"||"E"||"I"||"O"||"U"||"a"||"e"||"i"||"o"||"u"){
         printf("Consonant");
     }
     else {
