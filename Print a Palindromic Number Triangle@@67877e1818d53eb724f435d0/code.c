af1c19509d65b5ffe2413928a34a5a2e93131f0a
Ananya Bansal <ananyabansal3feb@gmail.com>
2025-02-22 15:49:51 +0530

    Solution to Print a Palindromic Number Triangle

--- Print a Palindromic Number Triangle@@67877e1818d53eb724f435d0/code.c
+++ Print a Palindromic Number Triangle@@67877e1818d53eb724f435d0/code.c
@@ -3,7 +3,7 @@ int main(){
     int a;
     scanf("%d",&a);
     for (int i=1;i<=a;i++){                   // Row loop 
-        for (int j=1;j<=n-i;j++){             // Printing spaces for alignment
+        for (int j=1;j<=a-i;j++){             // Printing spaces for alignment
             printf(" ");
         }
         for (int j=i;j>=1;j++){               // Printing decreasing numbers
